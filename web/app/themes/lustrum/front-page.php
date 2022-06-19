858037a11de0defb47877142f8e14e4f87fe8dc9
Max <maxklumper@outlook.com>
2022-06-19 23:11:50 +0200

    Theme update

--- web/app/themes/lustrum/front-page.php
+++ web/app/themes/lustrum/front-page.php
@@ -15,11 +15,7 @@
 
     <section class="main-content">
         <div class="container">
-            <h5 class="main-content__title">GST viert haar 5e verjaardag</h5>
-            <p>Dit jaar bestaat de vereniging 5 jaar en dat vieren wij in de Lustrumweek van 27 juni t/m 2 juli 2022.</p>
-            <p>We hebben verschillende activiteiten voor jullie in petto, van borrelen tot theater maken en/of zien.
-            Voor de precieze informatie wat betreft de activiteiten, check de pagina met ‘weekoverzicht’.</p>
-            <p>Wees erbij!</p>
+            <?php the_content(); ?>
         </div>
     </section>
 </main>
--- web/app/themes/lustrum/header.php
+++ web/app/themes/lustrum/header.php
@@ -12,12 +12,12 @@
 <header>
     <div class="container header">
         <div class="logo">
-            Logo
+            <img src="<?php bloginfo('stylesheet_directory') ?>/images/logo.svg" />
         </div>
 
         <div class="menu">
         <?php if (has_nav_menu('primary_navigation')) { ?>
-            <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
+            <nav class="nav-primary" aria-label="<?php wp_get_nav_menu_name('primary_navigation') ?>">
             <?php wp_nav_menu([
                 'theme_location' => 'primary_navigation',
                 'menu_class' => 'primary-navigation',
